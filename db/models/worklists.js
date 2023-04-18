c8096cb5112727514c319a172050ca9169c35ebb
Subhadip Dutta <subhadipdutta3000@gmail.com>
2023-04-18 09:42:36 +0530

    new project registration, new worktypes auto-add if new Project Type add while submitting new project, extra links removed from admin-nav, new field added as project_explore in projectlist database, project links made not required, accordingly index file also modified

--- db/models/worklists.js
+++ db/models/worklists.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const workSchema = mongoose.Schema({
-    work_code: {type: String, required: true, maxlength: 5},
+    work_code: {type: String, required: true, maxlength: 6},
     work_type: {type: String, required: true, maxlength: 50, unique: true},
 });
 
@@ -12,8 +12,9 @@ const projectSchema = mongoose.Schema({
     project_name: {type: String, required: true, maxlength: 50},
     project_ver: {type: String, required: true, maxlength: 15},
     project_desc: {type: String, required: true},
-    project_link: {type: String, required: true},
-    project_link_play: {type: String, required: true},
+    project_link: {type: String, required: false},
+    project_link_play: {type: String, required: false},
+    project_explore: {type: String, required: false}
 });
 
 
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const hbs = require('hbs');
 const bcrypt = require("bcryptjs");
 const cookieparser = require("cookie-parser");
+const slugify = require("slugify");
 
 // defining host and port
 const host = process.env.HOSTNAME || "http://127.0.0.1";
@@ -46,6 +47,33 @@ app.set('view engine', 'hbs');
 hbs.registerPartials(path.join(__dirname, "/partials"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+hbs.registerHelper('ifCond', function (v1, operator, v2, options) {
+    switch (operator) {
+        case '==':
+            return (v1 == v2) ? options.fn(this) : options.inverse(this);
+        case '===':
+            return (v1 === v2) ? options.fn(this) : options.inverse(this);
+        case '!=':
+            return (v1 != v2) ? options.fn(this) : options.inverse(this);
+        case '!==':
+            return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+        case '<':
+            return (v1 < v2) ? options.fn(this) : options.inverse(this);
+        case '<=':
+            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        case '>':
+            return (v1 > v2) ? options.fn(this) : options.inverse(this);
+        case '>=':
+            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+        case '&&':
+            return (v1 && v2) ? options.fn(this) : options.inverse(this);
+        case '||':
+            return (v1 || v2) ? options.fn(this) : options.inverse(this);
+        default:
+            return options.inverse(this);
+    }
+});
 
 
 
@@ -132,7 +160,7 @@ app.post("/admin-login", async(req, res) => {
 app.get("/admin/dashboard", async(req, res) => {
     if (req.cookies.admin) {
         let data_copy = JSON.parse(JSON.stringify(data));
-        data_copy["title"] = "Admin Login Panel";
+        data_copy["title"] = "Admin Panel";
         const admindata = await Admin.findOne({admin_email: req.cookies.admin});
         data_copy["admin_name"] = admindata.admin_name;
         data_copy["admin_email"] = admindata.admin_email;
@@ -170,17 +198,18 @@ app.get("/admin/projects", async(req, res) => {
     try {
         if (req.cookies.admin) {
             let data_copy = JSON.parse(JSON.stringify(data));
-            data_copy["title"] = "Admin Login Panel";
+            data_copy["title"] = "Admin Panel";
             const admindata = await Admin.findOne({admin_email: req.cookies.admin});
             data_copy["admin_name"] = admindata.admin_name;
             data_copy["admin_email"] = admindata.admin_email;
             data_copy["project_data"] = await ProjectList.find().sort({project_name: 1});
+            data_copy["work_list"] = await WorkList.find().sort({work_type: 1});
             res.render("admin/projects.hbs", data_copy);
         } else {
             res.redirect("/admin-login");
         }
     } catch (error) {
-        console.log(error)
+        res.status(400).send("Bad request");
     }
 })
 
@@ -189,19 +218,121 @@ app.get("/delete-project/:id", async(req, res) => {
     try {
         if (req.cookies.admin) {
             const delete_res = await ProjectList.deleteOne({_id: project_id});
+            console.log(delete_res);
+            res.redirect("/admin/projects");
+        } else {
+            res.redirect("/admin-login");
+        }
+    } catch (error) {
+        res.status(400).send("Bad request");
+    }
+})
+
+app.get("/update-project/:id", async(req, res) => {
+    const project_id = req.params.id;
+    try {
+        if (req.cookies.admin) {
             let data_copy = JSON.parse(JSON.stringify(data));
-            data_copy["title"] = "Admin Login Panel";
+            data_copy["title"] = "Admin Panel";
             const admindata = await Admin.findOne({admin_email: req.cookies.admin});
             data_copy["admin_name"] = admindata.admin_name;
             data_copy["admin_email"] = admindata.admin_email;
-            data_copy["project_data"] = await ProjectList.find().sort({project_name: 1});
-            console.log(delete_res);
-            res.render("admin/projects.hbs", data_copy);
+            data_copy["project_data"] = await ProjectList.findOne({_id: project_id});
+            data_copy["work_list"] = await WorkList.find().sort({work_type: 1});
+            for (let i = 0; i < data_copy.work_list.length; i++) {
+                data_copy["work_list"][i]["project_type"] = data_copy.project_data.work_type;
+            }
+            res.render("admin/update_project.hbs", data_copy);
+        } else {
+            res.redirect("/admin-login");
+        }
+    } catch (error) {
+        res.status(400).send("Bad request");
+    }
+})
+
+app.post("/update-project", async(req, res) => {
+    try {
+        if (req.cookies.admin) {
+            const p_id = req.body.p_id;
+            const p_type = req.body.p_type;
+            const p_slug = slugify(p_type, {lower: true});
+            const p_name = req.body.p_name;
+            const p_ver = req.body.p_ver;
+            const p_desc = req.body.p_desc;
+            const p_link = req.body.p_link;
+            const ps_link = req.body.ps_link;
+            const p_explore = req.body.p_explore;
+            
+            const update_res = await ProjectList.updateOne({_id: p_id}, {
+                $set: {
+                    work_type: p_type,
+                    work_slug: p_slug,
+                    project_name: p_name,
+                    project_ver: p_ver,
+                    project_desc: p_desc,
+                    project_link: p_link,
+                    project_link_play: ps_link,
+                    project_explore: p_explore
+                }
+            });
+            console.log(update_res);
+            res.redirect("/admin/projects");
+        } else {
+            res.redirect("/admin-login");
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(400).send("Bad request");
+    }
+});
+
+app.post("/submit-project", async(req, res) => {
+    try {
+        if (req.cookies.admin) {
+            const p_type = req.body.p_type;
+            const p_slug = slugify(p_type, {lower: true});
+            const p_name = req.body.p_name;
+            const p_ver = req.body.p_ver;
+            const p_desc = req.body.p_desc;
+            const p_link = req.body.p_link;
+            const ps_link = req.body.ps_link;
+            const p_explore = req.body.p_explore;
+            
+            const regProject = new ProjectList({
+                work_type: p_type,
+                work_slug: p_slug,
+                project_name: p_name,
+                project_ver: p_ver,
+                project_desc: p_desc,
+                project_link: p_link,
+                project_link_play: ps_link,
+                project_explore: p_explore
+            });
+            const registered = await regProject.save();
+            console.log(registered);
+            
+            // save new work type if not existed
+            const storedWorks = await WorkList.find();
+            let workFound = false;
+            for (let i = 0; i < storedWorks.length; i++) {
+                if (storedWorks[i].work_type == p_type) {
+                    workFound = true;
+                }
+            }
+            if (!workFound) {
+                const work_code = p_type.slice(0, 3).toUpperCase() + "DEV";
+                const regWork = new WorkList({work_code, work_type: p_type});
+                await regWork.save();
+            }
+
+            res.redirect("/admin/projects");
         } else {
             res.redirect("/admin-login");
         }
     } catch (error) {
         console.log(error);
+        res.status(400).send("Bad request");
     }
 })
 
