d5ee5cbdc5f9bc0af52a513e1fb85fe2199a8067
Subhadip Dutta <subhadip3105@gmail.com>
2025-02-27 16:30:25 +0530

    fixed allow blank jwt

--- functions/routers/interest_router.js
+++ functions/routers/interest_router.js
@@ -18,6 +18,10 @@ interestRouter.post("/", async (req, res) => {
         } = req.body;
 
         let signature = req.headers["x-appwrite-signature"];
+        if (!signature)
+            return res
+                .status(401)
+                .json({ error: "Appwrite signature not found in the header" });
 
         const interest = await createInterest({
             intr_icon,
@@ -48,6 +52,10 @@ interestRouter.get("/", async (req, res) => {
 
 interestRouter.put("/", async (req, res) => {
     let signature = req.headers["x-appwrite-signature"];
+    if (!signature)
+        return res
+            .status(401)
+            .json({ error: "Appwrite signature not found in the header" });
     try {
         const {
             bootstrap_icon_class: intr_icon,
@@ -70,6 +78,10 @@ interestRouter.put("/", async (req, res) => {
 
 interestRouter.delete("/", async (req, res) => {
     let signature = req.headers["x-appwrite-signature"];
+    if (!signature)
+        return res
+            .status(401)
+            .json({ error: "Appwrite signature not found in the header" });
     try {
         await deleteInterest(req.query.id, { signature });
         res.status(200).json({ message: "Interest data deleted successfully" });
